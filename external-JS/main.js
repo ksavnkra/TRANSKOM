f784638915f116f2b88f9f7e6f10eca2c18f64cf
narayani <narayanidogra@gmail.com>
2024-10-22 18:11:33 +0530

    merge

--- external-JS/main.js
+++ external-JS/main.js
@@ -12,4 +12,16 @@ var swiper = new Swiper(".slide-content", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints:{
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3 ,
+    }
+
+  },
 });
