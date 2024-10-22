0551cc47f55e68c4ff7f7a9e56a4c9da71ed6830
Keshav Nakra <Keshav10.nakra@gmail.com>
2024-10-22 17:56:48 +0530

    Merge

--- external-JS/main.js
+++ external-JS/main.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-var swiper = new swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-=======
 var swiper = new Swiper(".slide-content", {
   slidesPerView: 3,
   spaceBetween: 30,
@@ -30,4 +13,3 @@ var swiper = new Swiper(".slide-content", {
     prevEl: ".swiper-button-prev",
   },
 });
->>>>>>> e2ae07f37d0f5ddfda938d6188d136ad4c0b6e33
